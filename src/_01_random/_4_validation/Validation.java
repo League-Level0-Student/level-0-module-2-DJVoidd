797ec42e63f84b3d8fcd5c2ee6e1e14f824e5115
league <league@imac.attlocal.net>
2023-06-14 19:58:10 -0700

    random stuff (get it?)

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -12,12 +12,24 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+		int randomChoice = randomMaker.nextInt(5);
 		
-		System.out.println(randomNumber);
+		System.out.println(randomChoice);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomChoice == 0) {
+JOptionPane.showMessageDialog(null, "You are cool c:");
+		} else if (randomChoice == 1) {
+			JOptionPane.showMessageDialog(null, "You are not cool :c (it's opposite day)");
+		} else if (randomChoice == 2) {
+			JOptionPane.showMessageDialog(null, "you are chill");
+		} else if (randomChoice == 3) {
+			JOptionPane.showMessageDialog(null, "you have a below-room-tempurature heat level");
+		} else if (randomChoice == 4) {
+			JOptionPane.showMessageDialog(null, "your thermal energy is not up to standards");
+		} else {
+			JOptionPane.showMessageDialog(null, "you are feeling quite brisk today");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
